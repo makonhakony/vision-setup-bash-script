2d4317c08fa09a88da907eb2b58436f49aeeebc5
makonhakony <anphamhkn@gmail.com>
2023-07-27 16:43:17 -0700

    update script

--- vision-setup.sh
+++ vision-setup.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# Set the color variables
+# TODO
+
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install python3
 echo "Python Version: "
 python --version
 
-pip install virtualenv
-virtualenv robotenv
-source robotenv/bin/activate
+echo "Install coral necessary package"
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 
-pip install tensorflow
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install libedgetpu1-std
+
+sudo apt-get install python3-pycoral
+
+# vision packages
+# - pip packages
+sudo pip install tensorflow -y
+sudo pip install pillow -y
+sudo pip install matplotlib -y
+sudo pip install tensorflow-hub -y
+
+# - apt packages
+sudo apt-get install python3-picamera2 -y
 
 cat <<EOF > vision_version.py
 import tensorflow as tf
@@ -21,3 +39,9 @@ print("Numpy Version: ", np.__version__)
 EOF
 
 python vision_version.py
+
+sudo apt-get install libcamera-dev libcamera-utils libcamera-tests
+echo "Test camera"
+libcamera-hello
+
+echo "That is all, have fun"
